d89fc53082405d10b4665fbb946c0ee8f8f57b85
DYLANeay <dylan.eray6@gmail.com>
2026-03-09 15:21:13 +0100

    suii

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
 // Elements
-import './elements/artist-cover.js'
-import './elements/search-bar.js'
-import './elements/song-item.js'
-import './elements/spot-footer.js'
+import './elements/artist-cover.js';
+import './elements/search-bar.js';
+import './elements/song-item.js';
+import './elements/spot-footer.js';
 // Pages
-import './pages/page-artists.js'
-import './pages/page-home.js'
-import './pages/page-player.js'
-import './pages/page-songs.js'
+import './pages/page-artists.js';
+import './pages/page-home.js';
+import './pages/page-player.js';
+import './pages/page-songs.js';
 
 const router = () => {
-  const main = document.querySelector('main')
-  const hashs = (window.location.hash || '#home').split('/')
+	const main = document.querySelector('main');
+	const hashs = (window.location.hash || '#home').split('/');
 
-  if (hashs[0] == '#home')
-    main.innerHTML = '<page-home />'
+	if (hashs[0] == '#home') main.innerHTML = '<page-home />';
+	else if (hashs[0] == '#player') main.innerHTML = '<page-player />';
+	else if (hashs[0] == '#search' && hashs[1])
+		main.innerHTML = `<page-search-songs query="${hashs[1]}" />`;
+	else if (hashs[0] == '#artists' && hashs[1])
+		main.innerHTML = `<page-artist-songs artist-id="${hashs[1]}" />`;
+	else if (hashs[0] == '#artists' && !hashs[1])
+		main.innerHTML = '<page-artists />';
+	else if (hashs[0] == '#favorites') main.innerHTML = '<page-favorites />';
+};
 
-  else if (hashs[0] == '#player')
-    main.innerHTML = '<page-player />'
+window.addEventListener('hashchange', router);
 
-  else if (hashs[0] == '#search' && hashs[1])
-    main.innerHTML = `<page-search-songs query="${hashs[1]}" />`
-
-  else if (hashs[0] == '#artists' && hashs[1])
-    main.innerHTML = `<page-artist-songs artist-id="${hashs[1]}" />`
-
-  else if (hashs[0] == '#artists' && !hashs[1])
-    main.innerHTML = '<page-artists />'
-
-  else if (hashs[0] == '#favorites')
-    main.innerHTML = '<page-favorites />'
-}
-
-window.addEventListener('hashchange', router)
-
-router()
+router();
